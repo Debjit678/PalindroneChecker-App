558894e9b5c0f82d80f9e839eba4e2c515d47f9f
Debjit678 <dm9120@srmist.edu.in>
2026-02-24 16:28:12 +0530

    Palindrome Checker Started

---
+++ app/src/PalindromeChecker.java
@@ -0,0 +1,18 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        // Application Name
+        System.out.println("=================================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("=================================");
+
+        // Application Version
+        System.out.println("Version: 1.0.0");
+
+        // Continue to next use case or exit
+        System.out.println("\nStarting application...\n");
+
+        // Next use case will be called here
+    }
+}
